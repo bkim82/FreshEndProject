ff1db615c7a13e1898b4fb58bd538d477b9e181b
b_kim82 <brandonkim0802@gmail.com>
2019-05-30 12:32:47 -0400

    Hi

--- src/MiniGames.java
+++ src/MiniGames.java
@@ -1,8 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class MiniGames extends Application {
@@ -11,14 +17,22 @@ public class MiniGames extends Application {
 
         BorderPane bp = new BorderPane();
         Pane p = new Pane();
+        GridPane gp = new GridPane();
 
-        Label a = new Label("Mini Games!");
-        p.getChildren().add(a);
+        Label text = new Label("Mini Games!");
+        text.setFont(Font.font("Times Roman", FontWeight.BOLD, FontPosture.ITALIC, 50));
+        p.getChildren().add(text);
+
+        gp.add(new ImageView(new Image("image/og_image.jpg")), 0, 0);
+
         bp.setTop(p);
+        bp.setCenter(gp);
 
 
-        Scene scene = new Scene(p);
+        Scene scene = new Scene(bp,500,500);
         ps.setScene(scene);
         ps.show();
+        bp.requestFocus();
     }
 }
+
