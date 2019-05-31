10f77f68634e9616d0ac2a2634920fc92d6cafa8
b_kim82 <brandonkim0802@gmail.com>
2019-05-31 11:59:06 -0400

    Hi

--- src/MiniGames.java
+++ src/MiniGames.java
@@ -1,11 +1,11 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -17,19 +17,43 @@ public class MiniGames extends Application {
 
         BorderPane bp = new BorderPane();
         Pane p = new Pane();
-        GridPane gp = new GridPane();
+        HBox fp = new HBox(10);
+        HBox fp2 = new HBox(10);
+        fp.setAlignment(Pos.CENTER);
 
         Label text = new Label("Mini Games!");
         text.setFont(Font.font("Times Roman", FontWeight.BOLD, FontPosture.ITALIC, 50));
         p.getChildren().add(text);
 
-        gp.add(new ImageView(new Image("image/og_image.jpg")), 0, 0);
+        Image image = new Image("image/free-minesweeper-thumbnail.png");
+        ImageView view = new ImageView(image);
+        view.fitHeightProperty().bind(fp.heightProperty().divide(3));
+        view.fitWidthProperty().bind(fp.widthProperty().divide(3));
+
+        Image image2 = new Image("image/google-birthday-surprise-spinner.jpg");
+        ImageView view2 = new ImageView(image2);
+        view2.fitHeightProperty().bind(fp.heightProperty().divide(3));
+        view2.fitWidthProperty().bind(fp.widthProperty().divide(3));
+
+        Image image3 = new Image("image/simon.jpg");
+        ImageView view3 = new ImageView(image3);
+        view3.fitHeightProperty().bind(fp2.heightProperty().divide(3));
+        view3.fitWidthProperty().bind(fp2.widthProperty().divide(3));
+
+
+
+        fp.getChildren().addAll(view, view2);
+        fp2.getChildren().addAll(view3);
+
 
         bp.setTop(p);
-        bp.setCenter(gp);
+
+        bp.setCenter(fp);
+
+        bp.setBottom(fp2);
 
 
-        Scene scene = new Scene(bp,500,500);
+        Scene scene = new Scene(bp,700,500);
         ps.setScene(scene);
         ps.show();
         bp.requestFocus();
