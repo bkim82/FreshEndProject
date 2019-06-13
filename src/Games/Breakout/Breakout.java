2a9feec08429c03781a721b6113034aa4c13a7b0
b_kim82 <brandonkim0802@gmail.com>
2019-06-12 22:17:40 -0400

    Yo

--- src/Games/Breakout/Breakout.java
+++ src/Games/Breakout/Breakout.java
@@ -1,30 +1,31 @@
 package Games.Breakout;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+public class Breakout extends JFrame {
 
-public class Breakout {
+    public Breakout() {
 
-    public static final String title = "Breakout";
-    public static final String PADDLE_IMAGE = "paddle.gif";
-    public static final String TABLE = "background.jpg";
-    public static final String SLOW_SIGN = "slow.gif";
-    public static final int WIDTH = 400;
-    public static final int HEIGHT = 600;
-    public static final Paint BACKGROUND = Color.WHITE;
-    public static final int FRAMES_PER_SECOND = 60;
-    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    public static final int KEY_INPUT_SPEED = 25;
-    public static final double GROWTH_RATE = 1.1;
-    public static final int INITIAL_SPEED = 200;
+        initUI();
+    }
 
+    private void initUI() {
 
+        add(new Board());
+        setTitle("Breakout");
 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(Commons.WIDTH, Commons.HEIGTH);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
 
+    public static void main(String[] args) {
 
-
+        EventQueue.invokeLater(() -> {
+            Breakout game = new Breakout();
+            game.setVisible(true);
+        });
+    }
 }
-
-
--- src/Games/Breakout/regular_box.java
+++
@@ -1,9 +0,0 @@
-package Games.Breakout;
-
-public class regular_box extends BOX {
-
-    public regular_box(int width, int height, String type, int lives, boolean power) {
-
-        super(width, height, type, lives, power);
-    }
-}
